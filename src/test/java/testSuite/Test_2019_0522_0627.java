b721540f97a1bc24dae6e3631e8dc9fec7437458
cga1910 <cga1910@users.noreply.github.com>
2019-06-27 09:57:11 +0200

    Update Test_2019_0522_0627.java

--- src/test/java/testSuite/Test_2019_0522_0627.java
+++ src/test/java/testSuite/Test_2019_0522_0627.java
@@ -10,7 +10,8 @@ import org.testng.annotations.Test;
 import java.util.List;
 
 // TODO: 2019-06-27  Put all XPaths in the properties file?
-// TODO: 2019-06-27  Wrap lines at 80 columns.
+// TODO: 2019-06-27  Wrap lines at 80 columns
+// TODO: 2019-06-27  Get a better understanding of the events within XPath: //tag[text()[contains(.,'some text')]]
 
 public class Test_2019_0522_0627 extends Base_ {
 
